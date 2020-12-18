3ac1d8eda7afde30dad3063288af2230d11f9114
John Crites <johncritesdev@gmail.com>
2020-12-18 09:31:31 -0500

    toggle-logic-V1

--- public/scripts/app.js
+++ public/scripts/app.js
@@ -1,13 +1,34 @@
 'use strict';
 
-var template = React.createElement(
-  'div',
-  null,
-  React.createElement(
-    'h1',
-    null,
-    'Hello'
-  )
-);
+var message = false;
 
-ReactDOM.render(template, document.getElementById('root'));
+var onToggleBtn = function onToggleBtn() {
+  message = !message;
+  render();
+};
+
+var render = function render() {
+  var template = React.createElement(
+    'div',
+    null,
+    React.createElement(
+      'h1',
+      null,
+      'Toggle Logic'
+    ),
+    React.createElement(
+      'button',
+      { onClick: onToggleBtn },
+      message ? 'Hide Details' : 'Show Details',
+      'Toggle Button'
+    ),
+    message && React.createElement(
+      'p',
+      null,
+      'This appears when you click the toggle button and should disappear upon clicking it again.'
+    )
+  );
+  ReactDOM.render(template, document.getElementById('root'));
+};
+
+render();
--- src/app.js
+++ src/app.js
@@ -1,9 +1,24 @@
+let message = false;
 
+const onToggleBtn = () => {
+  message = !message;
+  render();
+}
 
-var template = (
-  <div>
-    <h1>Hello</h1>
-  </div>
-);
+const render = () => {
+  var template = (
+    <div>
+      <h1>Toggle Logic</h1>
+      <button onClick={onToggleBtn}>
+        {message ? 'Hide Details' : 'Show Details'}
+        Toggle Button
+        </button>
+      {message &&
+        <p>This appears when you click the toggle button and should disappear upon clicking it again.</p>}
+    </div>
+  );
+  ReactDOM.render(template, document.getElementById('root'));
+};
 
-ReactDOM.render(template, document.getElementById('root'));
+render();
+
